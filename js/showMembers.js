704940500f33bf481a6ef38bd878333a8972c95f
simba <simba.fs@gmail.com>
2019-07-09 21:26:37 +0800

    finish member

--- js/showMembers.js
+++ js/showMembers.js
@@ -1,18 +1,19 @@
 function showMember(n){
 	let template = 
 		`<div class="card bg-primary">
-			<div class="card-header" id="member$i">
-				<h2 class="mb-0">
-					<button class="card-title btn btn-link text-white" type="button" data-toggle="collapse" data-target="#collapse$i" aria-expanded="true" aria-controls="collapse$i">
-						$name
+				<h1 class="mb-0 card-header">
+					<button class="card-title btn btn-link text-white" type="button" data-toggle="collapse" data-target="#collapse$i$" aria-expanded="true" aria-controls="collapse$i$">
+						$name$
 					</button>
-				</h2>
-			</div>
+				</h1>
 
-			<div id="collapse$i" class="collapse" aria-labelledby="member$i" data-parent="#memberCollaspe">
-				<div class="card-body text-white">
-					<h4 class="card-subtitle text-center">$position</h4>
-					<img class="card-img-bottom img-fluid" src="$icon">
+			<div id="collapse$i$" class="collapse" aria-labelledby="member$i$" data-parent="#memberCollaspe">
+				<div class="card-body text-primary bg-light crad">
+					<div class="card-body">
+						<h4 class="card-title text-center">$position$</h4>
+						<img height="100" class="card-img-left img-fluid" src="$icon$">
+						<p class="card-text">$intro$</p>
+					</div>
 				</div>
 			</div>
 		</div>`;
@@ -22,13 +23,17 @@ function showMember(n){
 			let name = data[i].name;
 			let position = data[i].position;
 			let icon = data[i].avatar;
+			let intro = data[i].intro;
 			let person = template.
-				replace(/\$name/g, name).
-				replace(/\$position/g, position).
-				replace(/\$icon/g, icon).
-				replace(/\$i/g, i);
+				replace(/\$name\$/g, name).
+				replace(/\$position\$/g, position).
+				replace(/\$icon\$/g, icon).
+				replace(/\$i\$/g, i).
+				replace(/\$intro\$/g, intro);
 			to.append(person);
 		}
 	});
 }
 showMember(17);	
+
+
