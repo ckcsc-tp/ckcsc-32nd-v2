7d53f1eecaabe9270f35f6fbd4bb7fab71036a12
simba <simba.fs@gmail.com>
2019-07-10 08:56:34 +0800

    finish intro

--- js/showMembers.js
+++ js/showMembers.js
@@ -8,32 +8,41 @@ function showMember(n){
 				</h1>
 
 			<div id="collapse$i$" class="collapse" aria-labelledby="member$i$" data-parent="#memberCollaspe">
-				<div class="card-body text-primary bg-light crad">
-					<div class="card-body">
-						<h4 class="card-title text-center">$position$</h4>
-						<img height="100" class="card-img-left img-fluid" src="$icon$">
-						<p class="card-text">$intro$</p>
+				<div class="memberCard card-body text-primary bg-light">
+					<h4 class="card-title text-center">$position$</h4>
+					<div class="container-fluid">
+						<div class="row align-items-center">
+							<div class="col-md-4">
+								<img height="100" class="memberImg card-img-top img-fluid" src="$icon$">
+							</div>
+							<div class="memberContent col-md-8 text-center">
+								<p class="card-text">$intro$</p>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
 		</div>`;
 	$.getJSON('./json/members.json', data => {
 		let to = $('#memberCollaspe');
-		for(let i = 1; i <= n; i++){
-			let name = data[i].name;
-			let position = data[i].position;
-			let icon = data[i].avatar;
-			let intro = data[i].intro;
-			let person = template.
-				replace(/\$name\$/g, name).
-				replace(/\$position\$/g, position).
-				replace(/\$icon\$/g, icon).
-				replace(/\$i\$/g, i).
-				replace(/\$intro\$/g, intro);
-			to.append(person);
+		for(let i in data){
+			$.ajax(`./intro/${i}.txt`).
+				then(ajaxData => {
+					let name = data[i].name;
+					let position = data[i].position;
+					let icon = data[i].avatar;
+					let intro = ajaxData;
+					console.log(intro);
+					let person = template.
+						replace(/\$name\$/g, name).
+						replace(/\$position\$/g, position).
+						replace(/\$icon\$/g, icon).
+						replace(/\$i\$/g, i).
+						replace(/\$intro\$/g, intro);
+					to.append(person);
+				}).
+				catch(console.error);
 		}
 	});
 }
 showMember(17);	
-
-
